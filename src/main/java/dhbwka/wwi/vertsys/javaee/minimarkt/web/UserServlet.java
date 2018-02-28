546c295daec8ebbdeccb4595116a448dcd4428ef
MildL <MildL@zo018-n01.marksys.de>
2018-02-28 17:18:18 +0100

    User Servlet und user_edit erstellt

---
+++ src/main/java/dhbwka/wwi/vertsys/javaee/minimarkt/web/UserServlet.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright © 2018 Dennis Schulmeister-Zimolong
+ * 
+ * E-Mail: dhbw@windows3.de
+ * Webseite: https://www.wpvs.de/
+ * 
+ * Dieser Quellcode ist lizenziert unter einer
+ * Creative Commons Namensnennung 4.0 International Lizenz.
+ */
+package dhbwka.wwi.vertsys.javaee.minimarkt.web;
+
+import dhbwka.wwi.vertsys.javaee.minimarkt.ejb.CategoryBean;
+import dhbwka.wwi.vertsys.javaee.minimarkt.ejb.TaskBean;
+import dhbwka.wwi.vertsys.javaee.minimarkt.ejb.UserBean;
+import dhbwka.wwi.vertsys.javaee.minimarkt.ejb.ValidationBean;
+import dhbwka.wwi.vertsys.javaee.minimarkt.jpa.Category;
+import dhbwka.wwi.vertsys.javaee.minimarkt.jpa.Task;
+import dhbwka.wwi.vertsys.javaee.minimarkt.jpa.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author MildL
+ */
+
+@WebServlet(urlPatterns = {"/app/user/"})
+public class UserServlet extends HttpServlet {
+
+    @EJB
+    UserBean userBean;
+    
+    @EJB
+    TaskBean taskBean;
+
+    @EJB
+    ValidationBean validationBean;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet UserServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet UserServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+     @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Alle vorhandenen Kategorien ermitteln
+        request.setAttribute("user", userBean.getCurrentUser());
+        // Anfrage an dazugerhörige JSP weiterleiten
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/login/signup.jsp");
+        dispatcher.forward(request, response);
+
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Angeforderte Aktion ausführen
+        request.setCharacterEncoding("utf-8");
+        
+        String action = request.getParameter("action");
+
+        if (action == null) {
+            action = "";
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    
+    public void update (User user) {
+        userBean.update(user);
+    }
+
+}
--- src/main/webapp/WEB-INF/app/user_edit.jsp
+++ src/main/webapp/WEB-INF/app/user_edit.jsp
@@ -1,17 +1,59 @@
 <%-- 
     Document   : user_edit
-    Created on : 28.02.2018, 12:11:37
+    Created on : 28.02.2018, 16:21:35
     Author     : MildL
 --%>
-
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
-    </head>
-    <body>
-        <h1>Hello World!</h1>
-    </body>
-</html>
+
+<%@taglib tagdir="/WEB-INF/tags/templates" prefix="template"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+
+<template:base>
+    <jsp:attribute name="title">
+        User bearbeiten
+    </jsp:attribute>
+
+    <jsp:attribute name="head">
+        <link rel="stylesheet" href="<c:url value="/css/user_edit.css"/>" />
+    </jsp:attribute>
+
+    <jsp:attribute name="menu">
+        <div class="menuitem">
+            <a href="<c:url value="/app/tasks/"/>">Übersicht</a>
+        </div>
+    </jsp:attribute>
+
+    <jsp:attribute name="content">
+        <form method="post" class="stacked">
+            <%-- CSRF-Token --%>
+            <input type="hidden" name="csrf_token" value="${csrf_token}">
+            
+            <%-- Vorhandene Kategorien --%>
+            <c:choose>
+                <c:when test="${empty categories}">
+                    <p>
+                        Es sind noch keine Kategorien vorhanden. 🐏
+                    </p>
+                </c:when>
+                <c:otherwise>
+                    <div>
+                        <div class="margin">
+                            <c:forEach items="${categories}" var="user">
+                                <input type="checkbox" name="user" id="${'category-'.concat(user.id)}" value="${user.id}" />
+                                <label for="${'user-'.concat(user.id)}">
+                                    <c:out value="${user.name}"/>
+                                </label>
+                                <br />
+                            </c:forEach>
+                        </div>
+
+                        <button type="submit" name="action" value="update" class="icon-trash">
+                            Aktualisieren
+                        </button>
+                    </div>
+                </c:otherwise>
+            </c:choose>
+        </form>
+    </jsp:attribute>
+</template:base>
